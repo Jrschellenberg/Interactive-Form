bcd94eccc2d1741ce013234de5b98afe211c0e2f
Justin Schellenberg <jrschellenberg@Gmail.com>
2017-01-09 19:35:13 -0600

    Did some commenting, Tidying

--- js/main.js
+++ js/main.js
@@ -1,27 +1,21 @@
 /**
  * Created by Justin on 1/4/2017.
+ * using the Jquery plugin Form validator from http://www.formvalidator.net/index.html
  */
 jQuery(function ($) {
 
+
+	//Code for custom function for jquery validator
 	$.formUtils.addValidator({
 		name : 'checkboxes',
 		validatorFunction : function(value, $el, config, language, $form) {
-			return $('input[type="checkbox"]:checked').length > 0;
+			return $('input[type="checkbox"]:checked').length > 0; //Making sure at least one checkbox is selected.
 		},
 		errorMessage : 'You need to select at least one checkbox',
 		errorMessageKey: 'uncheckedBox'
 	});
 
-	$.formUtils.addValidator({
-		name : 'shirtSelected',
-		validatorFunction : function(value, $el, config, language, $form) {
-			return $('#design').val() == "js puns" || $('#design').val() == "heart js";
-		},
-		errorMessage : 'Dont forget to pick a T-Shirt',
-		errorMessageKey: 'noShirtSelected'
-	});
-
-
+	//Adding the security module from the plugin for credit card verification.
 	$.validate({
 		modules : 'security', //Adding the module for creditcard Validation
 		validateOnBlur : true, // disable validation when input looses focus
@@ -48,16 +42,12 @@ jQuery(function ($) {
 	paymentArray[1] = $("#credit-card").next();
 	paymentArray[2] = $("#credit-card").next().next();
 
-	//Hiding all payment options on pageload.
+	//Hiding all payment options on page load.
 	hideShowOptions(paymentArray, -1);
-
-
-
-
 
 	//setting variable for the color select id
 	var colorElement = $("#colors-js-puns");
-	//hiding the element on page load.
+	//hiding the color select box on page load.
 	colorElement.hide();
 
 
@@ -72,9 +62,9 @@ jQuery(function ($) {
 	});
 
 	$("#design").change(function () {
+		//making variable of the option selected on change.
 		var optionSelected = $("#design option:selected").val();
 		$("#color option").css("display","block");
-		//console.log(optionSelected);
 		if(optionSelected == "js puns"){
 			//show the element.
 			//Call the setOptions function to set the specified option to default selected
@@ -93,11 +83,13 @@ jQuery(function ($) {
 		}
 	});
 
+
+	//Called when someone makes a change with the select payment element with id of #payment
 	$("#payment").change(function(){
+		//Grabs the current option selected
 		var optionSelected = $("#payment option:selected").val();
 
-		console.log(paymentArray);
-
+		//Logic to test which options to show, and which to hide.
 		if(optionSelected == "credit card"){
 			hideShowOptions(paymentArray, 0);
 		}
@@ -111,41 +103,28 @@ jQuery(function ($) {
 			//if the 3 options aren't selected. Hide all options.
 			hideShowOptions(paymentArray, -1);
 		}
-
-
 	});
 
+	//Called when detecting a change for any of the activities checkboxes
 	$(".activities label").change(function(){
-
+		//Grab the string of text of the current label of checkbox checked.
 		var str = $(this).text();
+		//Calling the function getSubString to process the string into a substring containing the time if it exists.
 		var timeString = getSubString(str);
 
 		if($(this).children().is(":checked"))
 		{
+			//Calling the function enableDisableCheckboxes, see below (function is a nested function!)
 			enableDisableCheckboxes(true);
 		}else{
 			enableDisableCheckboxes(false);
 		}
 
+
 		/*
-		A function to give checkboxes the disabled or enabled attribute based on the
-		time contained within their string
-		disableBox, Boolean: if true will disable the boxes, if false will disable the boxes.
+		The block of code Below here in this change function is used to make a running total of
+		 the cost of activities
 		 */
-		function enableDisableCheckboxes(disableBox){
-			$(".activities input[type=checkbox]").each(function () {
-				var isChecked = $(this).is(":checked");
-				var str = $(this).closest("label").text();
-				var comparedTimeString = getSubString(str);
-				if (!isChecked) {
-					if (comparedTimeString == timeString) {
-						$(this).prop("disabled", disableBox);
-					}
-				}
-			});
-		}
-
-
 
 		//Splitting the String of the currently clicked label element at the $,
 		//limiting to only 2 array values
@@ -164,14 +143,33 @@ jQuery(function ($) {
 			totalAmountSelected-= moneyValue;
 		}
 		$("#totalAmount").text(totalAmountSelected);
+
+		/*
+		 A function to give checkboxes the disabled or enabled attribute based on the
+		 time contained within their string
+		 disableBox, Boolean: if true will disable the boxes, if false will disable the boxes.
+		 */
+		function enableDisableCheckboxes(disableBox){
+			$(".activities input[type=checkbox]").each(function () {
+				var isChecked = $(this).is(":checked");
+				var str = $(this).closest("label").text();
+				var comparedTimeString = getSubString(str);
+				if (!isChecked) {
+					if (comparedTimeString == timeString) {
+						$(this).prop("disabled", disableBox);
+					}
+				}
+			});
+		}
 	});
 
+	/*
+	A function used to get the substring of text, gets the substring contained between — and ,
+	str: the input string to grab the substring of.
+	 */
 	function getSubString(str){
 		return str.substring(str.lastIndexOf("— ")+1, str.lastIndexOf(","));
 	}
-
-
-
 
 	/*
 	A function used to set and remove the attributed selector. As well as setting the
@@ -188,7 +186,11 @@ jQuery(function ($) {
 		$("#color option:nth-child("+formula+")").css("display","none");
 	}
 
-
+	/*
+	A function to hide and show elements of an array
+	array:an array of dom elements to hide or show
+	showIndex, the index of the array, used to select which element of the array to show. rest become hidden.
+	 */
 	function hideShowOptions(array, showIndex){
 		for(var i=0; i<array.length; i++){
 			if(i==showIndex){
@@ -199,6 +201,4 @@ jQuery(function ($) {
 			}
 		}
 	}
-
-
 });
